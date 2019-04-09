89942d26919c03aedf530f8d002a1b632b0cc1fe
Jackson Paul <c3183717@uon.edu.au>
2019-04-09 11:16:59 +0000

    Getting familiar with Che
    Added Opening Comments

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/*
+ * Name:Jackson Paul
+ * Subject: ENGG1003
+ * Assessment: Project 1
+ * Description:
+ * 1. Encryption of a message with a rotation cipher given the message text and rotation amount
+ * 2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
+ * 3. Encryption of a message with a substitution cipher given message text and alphabet substitution
+ * 4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions
+ * 5. Decryption of a message encrypted with a rotation cipher given cipher text only
+ * 6. Decryption of a message encrypted with a substitution cipher given cipher text only
+ * Please take note of commenting throughout the code for ongoing explanation.
+ */
+
+
 #iclude <stdio.h>
 
 int main() {
